b24fb08364d6bac7cf5cc200805f939f41fa8d0d
SG_Muwa <motherlode.muwa@gmail.com>
2019-10-28 23:47:42 +0300

    Simplex: Исправлены названия.

--- Simplex/Simplex.cs
+++ Simplex/Simplex.cs
@@ -6,7 +6,7 @@ namespace Simplex
     /// <summary>
     /// Реализация симплекса.
     /// </summary>
-    public class Simplex
+    public static class Simplex
     {
         /// <summary>
         /// Запускает завимодействие пользователя с симплекс-методом.
@@ -22,79 +22,65 @@ namespace Simplex
             matrix = new double[N + 3, perem + 3]; // Строк, столбцов
             matrix2 = new double[N, perem]; // Строк, столбцов
             List<double> list = new List<double>();
-            List<double> list2 = new List<double>();
+            IList<double> list2 = new double[N];
             Console.WriteLine("Введите элементы симплекс таблицы по строкам! Для решения исходной задачи: ");
-            for (int i = 0; i < N + 3; i++) {
-                for (int j = 0; j < perem + 3; j++) { // Столбцы
-                    if (!double.TryParse(Console.ReadLine(), out matrix[i, j]))
+            for (int y = 0; y < N + 3; y++) {
+                for (int x = 0; x < perem + 3; x++) { // Столбцы
+                    if (!double.TryParse(Console.ReadLine(), out matrix[y, x]))
                     {
                         Console.WriteLine("Вы ввели не корректное число");
-                        i--;
+                        y--;
                     }
                 }
             }
-            for (int i = 0; i < perem; i++) //Коэффициенты целевой функции
-                list.Add(matrix[0, i + 2]);
-            for (int i = 0; i < N; i++) //свободные
-                list2.Add(matrix[i + 2, perem + 2]);
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < perem; j++)
-                    matrix2[i, j] = matrix[i + 2, j + 2];
-            Console.WriteLine("");Console.WriteLine("");
+            for (int x = 0; x < perem; x++) //Коэффициенты целевой функции
+                list.Add(matrix[0, x + 2]);
+            for (int y = 0, i = 0; y < N; y++) //свободные
+                list2[i++] = matrix[y + 2, perem + 2];
+            for (int y = 0; y < N; y++)
+                for (int x = 0; x < perem; x++)
+                    matrix2[y, x] = matrix[y + 2, x + 2];
+            Console.WriteLine("\n");
             int tmpPerem = perem;
             int index = 1;
             int jndex = 0;
             while (tmpPerem != 0) {
-                double delta = delta_computing(matrix, index++, N, perem);
+                double delta = GetDelta(matrix, index++, N, perem);
                 matrix[N + 3 - 1, jndex + 2] = delta;
                 tmpPerem--;
                 jndex++;
-                double Q = delta_computing(matrix, 0, N, perem);//A0
+                double Q = GetDelta(matrix, 0, N, perem); // A0
                 matrix[N + 3 - 1, perem + 3 - 1] = Q;
             }
             Console.WriteLine("Решение исходной задачи симплекс-методом:");
-            int flag;
-            do
-            {
-                flag = 0;
-                for (int k = 0; k < perem; k++)
-                    if (matrix[N + 3 - 1, k + 2] < 0)
-                        flag = 1;
-                if (flag == 1)
+            for (int k = 0; k < perem; k++)
+                if (matrix[N + 3 - 1, k + 2] < 0)
                 {
-                    int razreshColumn = razresh_column(matrix, N, perem);
-                    int razreshString = razresh_string(matrix, razreshColumn, N, perem);
-                    matrix = new_simpleks_matrix(matrix, N + 3, perem + 3, razreshColumn, razreshString);
-                }
-                if (flag == 1)
+                    int razreshColumn = PermitColumn(matrix, N, perem);
+                    int razreshString = PermitString(matrix, razreshColumn, N, perem);
+                    matrix = NewSimplexMatrix(matrix, N + 3, perem + 3, razreshColumn, razreshString);
                     for (int d = 0; d < N + 3; d++)
                     {
                         for (int t = 0; t < perem + 3; t++)
-                        {
                             Console.Write(matrix[d, t] + "\t");
-                        }
                         Console.WriteLine();
                     }
-                Console.WriteLine();
-                Console.WriteLine();
-            } while (flag == 1);
+                    Console.WriteLine("\n");
+                    k = -1;
+                    continue;
+                }
 
-
-            Console.WriteLine("Коэффициенты целевой функции для двойственной задачи:");
-            for (int t = 0; t < N; t++) //свободные
-                Console.Write(list2[t]+"  ");
-            Console.WriteLine();
-            Console.WriteLine("Транспонированная матрица для двойственной задачи:");
+            Console.WriteLine($"Коэффициенты целевой функции для двойственной задачи:\n"
+                + $"{string.Join("  ", list2)}\n\n" // Свободные
+                + $"Транспонированная матрица для двойственной задачи:");
             double[,] transMatrix = new double[perem, N];
-            transMatrix = transposeMatrix(matrix2);
+            transMatrix = matrix2.Transpose();
             for (int t = 0; t < perem; t++) {
                 for (int m = 0; m < N; m++)
-                    Console.Write(transMatrix[t, m] + "  ");
+                    Console.Write($"{transMatrix[t, m], 6 : N4}");
                 Console.WriteLine();
             }
-            Console.WriteLine("Свободные коэфициенты для двойственной задачи:");
-            for(int t=0;t<list.Count;t++)
-                Console.Write(list[t]+"  ");
+            Console.WriteLine($"Свободные коэфициенты для двойственной задачи:\n{string.Join("  ", list)}");
             int forlist3 = 0;
             Console.WriteLine("\n\nБазисные переменные из последней симплекс таблицы:");
             List<int> list3 = new List<int>();
@@ -104,18 +90,15 @@ namespace Simplex
                 list3.Add(forlist3);
                 Console.Write(list3[k] + " ");
             }
-            Console.WriteLine("");
-            double max_dohod = matrix[N + 2, perem + 2];//ответ прибыль 53
+            Console.WriteLine();
+            double max_profit = matrix[N + 2, perem + 2]; // ответ прибыль 53
             int numBasisPerem = N; double[,] matrixBasis;
-            matrixBasis = new double[numBasisPerem, numBasisPerem];//строк, столбцов
-            for (int t = 0; t < numBasisPerem; t++) {
-                for (int k = 0; k < numBasisPerem; k++) {
-                    if (k == t) {
-                        matrixBasis[t, k] = 1;
-                    } else {
-                        matrixBasis[t, k] = 0; }
-                }
-            }
+            matrixBasis = new double[numBasisPerem, numBasisPerem]; // строк, столбцов
+            for (int t = 0; t < numBasisPerem; t++)
+                for (int k = 0; k < numBasisPerem; k++)
+                    matrixBasis[t, k] = k == t
+                        ? 1
+                        : 0;
             int tempIndex = -1; double[,] matrixD;
             matrixD = new double[N, N];//строк, столбцов
             for (int t = 0; t < N; t++)
@@ -145,21 +128,22 @@ namespace Simplex
                     Console.Write(matrixD[d, t] + "\t");
                 Console.WriteLine();
             }
-            Console.WriteLine(); Console.WriteLine("Обратная Матрица D");
-            inversion(matrixD, N);
-            for (int d = 0; d < N; d++) {
-                for (int t = 0; t < N; t++)
-                    Console.Write(matrixD[d, t] + "\t");
+            Console.WriteLine("\nОбратная Матрица D");
+            matrixD = matrixD.Inversion();
+            for (int y = 0; y < matrixD.GetLength(0); y++) {
+                for (int x = 0; x < matrixD.GetLength(1); x++)
+                    Console.Write(matrixD[y, x] + "\t");
                 Console.WriteLine();
             }
-            list3.Clear();//базисный вектор 5 3 0 0
+            list3.Clear(); // базисный вектор 5 3 0 0
             Console.WriteLine("Базисиный вектор");
             for (int d = 0; d < N; d++) {
                 int tmp = (int) matrix[d + 2, 0];
                 list3.Add(tmp);
                 Console.Write(list3[d] + "  ");
             }
-            Console.WriteLine();double result = 0;
+            Console.WriteLine();
+            double result = 0;
             List<double> list4 = new List<double>();
             for (int t = 0; t < N; t++) {
                 for (int k = 0; k < N; k++)
@@ -173,18 +157,18 @@ namespace Simplex
             for (int t = 0; t < N; t++)
                 result += list2[t] * list4[t];
             Console.WriteLine("Получившийся вектор умножаем на свободные коэфициенты прямой задачи. Результат для Gmin: " + result);
-            Console.WriteLine("Результат для Fmax: " + max_dohod);
+            Console.WriteLine("Результат для Fmax: " + max_profit);
         }
 
         /// <summary>
-        /// Расчет относительных оценок и оптимального плана.
+        /// Расчёт относительных оценок и оптимального плана.
         /// </summary>
         /// <param name="matrix"></param>
         /// <param name="a"></param>
         /// <param name="N"></param>
         /// <param name="perem"></param>
         /// <returns></returns>
-        public static double delta_computing(double[,] matrix, int a, int N, int perem)
+        public static double GetDelta(double[,] matrix, int a, int N, int perem)
         {
             double delta = 0;
             if (a != 0) {
@@ -205,7 +189,7 @@ namespace Simplex
         /// <param name="N"></param>
         /// <param name="perem"></param>
         /// <returns></returns>
-        public static int razresh_column(double[,] matrix, int N, int perem)
+        private static int PermitColumn(double[,] matrix, int N, int perem)
         {
             double[] mas;
             mas = new double[perem];
@@ -225,7 +209,12 @@ namespace Simplex
         /// <summary>
         /// Нахождение разрешающей строки.
         /// </summary>
-        public static int razresh_string(double[,] matrix, int razreshColumn, int N, int perem) {
+        /// <param name="matrix"></param>
+        /// <param name="razreshColumn">Индекс разрешающего столбца.</param>
+        /// <param name="N"></param>
+        /// <param name="perem"></param>
+        /// <returns></returns>
+        private static int PermitString(double[,] matrix, int razreshColumn, int N, int perem) {
             double[] mas; mas = new double[N + 2]; mas[0] = -0; mas[1] = -0;
             double min_elem = 0; int index_min_elem;
             for (int i = 2; i < N + 2; i++)
@@ -238,78 +227,87 @@ namespace Simplex
                 }
             return index_min_elem;
         }
-        public static double[,] new_simpleks_matrix(double[,] matrix, int strSipleks, int colum, int razreshColumn, int razreshString)
+
+        
+        private static double[,] NewSimplexMatrix(double[,] matrix, int strSipleks, int colum, int permitColumn, int permitString)
         {
             double[,] matrix2;
             matrix2 = new double[strSipleks, colum];
             for (int i = 0; i < strSipleks; i++)
                 for (int j = 0; j < colum; j++)
                     matrix2[i, j] = matrix[i, j];
-            matrix2[razreshString, 0] = matrix[0, razreshColumn];
-            matrix2[0, razreshColumn] = matrix[razreshString, 0];
-            matrix2[razreshString, 1] = matrix[1, razreshColumn];
-            matrix2[1, razreshColumn] = matrix[razreshString, 1];
-            double razreshElemetn = matrix[razreshString, razreshColumn];
+            matrix2[permitString, 0] = matrix[0, permitColumn];
+            matrix2[0, permitColumn] = matrix[permitString, 0];
+            matrix2[permitString, 1] = matrix[1, permitColumn];
+            matrix2[1, permitColumn] = matrix[permitString, 1];
+            double razreshElemetn = matrix[permitString, permitColumn];
             for (int k = 2; k <= colum - 1; k++) //разрешающая строка (просмотр столбцов) было к = 4
             {
-                if (k != razreshColumn) {
-                    matrix2[razreshString, k] = matrix[razreshString, k] / razreshElemetn;
+                if (k != permitColumn) {
+                    matrix2[permitString, k] = matrix[permitString, k] / razreshElemetn;
                 }
             }
             for (int k = 2; k <= strSipleks - 1; k++) //разрешающая столбец (просмотр строк) было к = 6
             {
-                if (k != razreshString) {
-                    matrix2[k, razreshColumn] = (-1) * (matrix[k, razreshColumn] / razreshElemetn);
+                if (k != permitString) {
+                    matrix2[k, permitColumn] = (-1) * (matrix[k, permitColumn] / razreshElemetn);
                 }
             }
-            matrix2[razreshString, razreshColumn] = 1 / razreshElemetn;
+            matrix2[permitString, permitColumn] = 1 / razreshElemetn;
             for (int str = 2; str < strSipleks; str++)
                 for (int col = 2; col < colum; col++) //столбцы  было col < 5
-                    if (str != razreshString && col != razreshColumn)
-                        matrix2[str, col] = (matrix[str, col] * razreshElemetn - matrix[razreshString, col] * matrix[str, razreshColumn]) / razreshElemetn;
+                    if (str != permitString && col != permitColumn)
+                        matrix2[str, col] = (matrix[str, col] * razreshElemetn - matrix[permitString, col] * matrix[str, permitColumn]) / razreshElemetn;
             return matrix2;
         }
-        private static void inversion(double[,] A, int N)
+
+
+        public static double[,] Inversion(this double[,] input)
         {
-            double temp;double[,] E = new double[N, N];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                {
-                    E[i, j] = 0f;
-                    if (i == j)
-                        E[i, j] = 1f;
-                }
-            for (int k = 0; k < N; k++) {
-                temp = A[k, k];
-                for (int j = 0; j < N; j++) {
-                    A[k, j] /= temp;
+            double[,] output = (double[,])input.Clone();
+            double temp;
+            double[,] E = new double[output.GetLength(0), output.GetLength(1)];
+            for (int i = 0; i < E.GetLength(0); i++)
+                for (int j = 0; j < E.GetLength(1); j++)
+                    E[i, j] = i == j
+                        ? 1
+                        : 0;
+            for (int k = 0; k < E.GetLength(0); k++) {
+                temp = output[k, k];
+                for (int j = 0; j < E.GetLength(1); j++) {
+                    output[k, j] /= temp;
                     E[k, j] /= temp;
                 }
-                for (int i = k + 1; i < N; i++) {
-                    temp = A[i, k];
-                    for (int j = 0; j < N; j++) {
-                        A[i, j] -= A[k, j] * temp;
+                for (int i = k + 1; i < E.GetLength(0); i++) {
+                    temp = output[i, k];
+                    for (int j = 0; j < E.GetLength(1); j++) {
+                        output[i, j] -= output[k, j] * temp;
                         E[i, j] -= E[k, j] * temp;
                     }
                 }
             }
-            for (int k = N - 1; k > 0; k--) {
+            for (int k = E.GetLength(0) - 1; k > 0; k--) {
                 for (int i = k - 1; i >= 0; i--) {
-                    temp = A[i, k];
-                    for (int j = 0; j < N; j++) {
-                        A[i, j] -= A[k, j] * temp;
+                    temp = output[i, k];
+                    for (int j = 0; j < E.GetLength(1); j++) {
+                        output[i, j] -= output[k, j] * temp;
                         E[i, j] -= E[k, j] * temp;
-                    }}}
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    A[i, j] = E[i, j];
+                    }
                 }
             }
+            for (int i = 0; i < E.GetLength(0); i++)
+                for (int j = 0; j < E.GetLength(1); j++)
+                    output[i, j] = E[i, j];
+            return output;
         }
 
-     private static double[,] transposeMatrix(double [, ] m)
-     {
+        /// <summary>
+        /// Транспонирование исходной матрицы.
+        /// </summary>
+        /// <param name="m">Матрица, которую надо транспортировать.</param>
+        /// <returns>Транспонированная матрица.</returns>
+        public static double[,] Transpose(this double[,] m)
+        {
             double[,] temp = new double[m.GetLength(1), m.GetLength(0)];
             for (int i = 0; i < m.GetLength(0); i++)
                 for (int j = 0; j < m.GetLength(1); j++)
